a18d610f742b72e51cdedd44beef4d4cfe3baf9e
Hassan Majeed <36446500+blank-del@users.noreply.github.com>
2026-01-17 14:41:22 +0200

    Added dbt dag

--- analytics/models/marts/fct_orders.sql
+++ analytics/models/marts/fct_orders.sql
@@ -1,10 +1,10 @@
 select
-    c.order_id,
-    c.customer_id,
-    c.price,
-    c.currency,
-    c.created_at,
-    co.completed_at,
+    c.order_id as order_id,
+    c.customer_id as customer_id,
+    c.price as price,
+    c.currency as currency,
+    c.created_at as created_at,
+    co.completed_at as completed_at,
     co.status
 from {{ ref('stg_order_created') }} c
 left join {{ ref('stg_order_completed') }} co
